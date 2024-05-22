5e1eef4b9f245b030b481a8e2efc63afe1fd87be
Thomas Barr <thomasrbthree@gmail.com>
2024-05-22 16:04:29 -0400

    Update honeyd_install.sh
    Visual cleanup of steps

--- honeyd_install.sh
+++ honeyd_install.sh
@@ -6,14 +6,16 @@ sudo apt-get update
 sudo apt-get install libevent-dev libdumbnet-dev libpcap-dev libpcre3- dev libedit-dev bison flex libtool automake
 
 # Step 2: Install Git
-echo "Installing Git..." sudo apt-get install -y git
+echo "Installing Git..." 
+sudo apt-get install -y git
 
 # Step 3: Clone Honeyd
 echo "Cloning Honeyd from GitHub..."
 git clone https://github.com/Unbiseptium/Honeyd
 
 # Step 4: Build Honeyd
-echo "Building and installing Honeyd..." cd Honeyd || exit
+echo "Building and installing Honeyd..." 
+cd Honeyd || exit
 ./autogen.sh
 ./configure
 make
